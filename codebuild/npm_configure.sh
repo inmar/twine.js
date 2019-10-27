3029cb0812e3209c955875347e1d69df342424bd
Austin Keener <Keeneraustin@yahoo.com>
2019-10-27 19:37:04 -0400

    Cleaned up npm_configure and added npmjs.org registry auth line.

--- codebuild/npm_configure.sh
+++ codebuild/npm_configure.sh
@@ -13,23 +13,16 @@ assert_variable_exists() {
 
 echo "Setting up NPM config..."
 assert_variable_exists "NEXUS_NAME"
-assert_variable_exists "NEXUS_USERNAME"
-assert_variable_exists "NEXUS_PASSWORD"
 assert_variable_exists "NEXUS_EMAIL"
+assert_variable_exists "NPM_TOKEN"
 assert_variable_exists "GITHUB_TOKEN"
 
-### Setup .npmrc for Nexus
 NPMRC="
-## Not currently deploying to nexus, so don't set these values.
-# registry=https://nexus.inmar.com/nexus/content/repositories/npm-all/
-# _auth=$(echo -n $NEXUS_USERNAME:$NEXUS_PASSWORD | openssl base64)
-
-@inmar:registry=https://npm.pkg.github.com/
+//registry.npmjs.registry:_authToken=$NPM_TOKEN
 //npm.pkg.github.com/:_authToken=$GITHUB_TOKEN
 
 init.author.name=$NEXUS_NAME
 init.author.email=$NEXUS_EMAIL
 email=$NEXUS_EMAIL
-always-auth=true
 progress=false"
 echo "$NPMRC" > ~/.npmrc
